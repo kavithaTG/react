874b73944cd5450f97d4d3dd6c0fc0906738207b
kavithatg <kavithakavi11748@gmail.com>
2023-02-25 12:44:05 +0530

    puhed

---
+++ example.js
@@ -0,0 +1,27 @@
+
+console.log("a"+10);
+console.log(true+"hi");
+console.log(true+true);
+console.log(true+20);
+console.log(true-21);
+console.log(true-"hi");
+console.log(true-"20");
+console.log("hi"-"hi");
+console.log(typeof(null));
+console.log(typeof(window));
+console.log(typeof(dom));
+console.log(typeof(NaN));
+console.log(typeof(undefined));
+console.log(typeof(Object));
+console.log(typeof(Math));
+console.log(typeof(bom));
+console.log(typeof(console));
+console.log(null+"hi");
+console.log(null+10);
+console.log(null-"10");
+console.log(null+null);
+console.log(null+true);
+var a=10;
+console.log(a);
+
+log
