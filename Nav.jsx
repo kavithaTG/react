609ede0ffefbf37916cc1703fa420f2d4c15847e
kavithaTG <126230371+kavithaTG@users.noreply.github.com>
2023-03-08 20:51:03 +0530

    Add files via upload

--- Nav.jsx
+++ Nav.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 const Nav=(props)=>{
     return(
         <>
-        <img src={props.data} alt="" />
+        {/* <img src={props.data} /> */}
+        {/* <img src={props.data}/> */}
+        <h1 style={{color:"yellow",backgroundColor:"black"}}>{props.data.username} {props.data.password}</h1>
         </>
     )
 }
