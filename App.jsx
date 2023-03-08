7f8d40e9d83dad417ee58ca01a07f65444abd5f5
kavithaTG <126230371+kavithaTG@users.noreply.github.com>
2023-03-08 20:23:08 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav";
+
+
+const App=()=>{
+    return(
+        <>
+        <Nav data={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiBZSHpER02qdROeHLYq8lzbITfmfvlFAdY1BAQIDzMeZGCqifSwY3rNRK1Tepl2vzr7E&usqp=CAU"}/>
+        {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyz-t9qMRSJEXFghsW9y1mUnmtJE3A8TYs3g&usqp=CAU" alt="" /> */}
+        </>
+    )
+}
+export default App;
