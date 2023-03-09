e0a36eca471ddd04321dadfdbf619f169ed1678c
kavithaTG <126230371+kavithaTG@users.noreply.github.com>
2023-03-09 13:30:30 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Nav from "./Nav";
-
+import pic from "./picture.avif"
 
 const App=()=>{
     return(
         <>
-        <Nav data={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiBZSHpER02qdROeHLYq8lzbITfmfvlFAdY1BAQIDzMeZGCqifSwY3rNRK1Tepl2vzr7E&usqp=CAU"}/>
-        {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyz-t9qMRSJEXFghsW9y1mUnmtJE3A8TYs3g&usqp=CAU" alt="" /> */}
+        {/* <Nav data={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiBZSHpER02qdROeHLYq8lzbITfmfvlFAdY1BAQIDzMeZGCqifSwY3rNRK1Tepl2vzr7E&usqp=CAU"}/> */}
+        {/* <Nav data={pic} /> */}
+        {/* <Nav data={{
+              username:"rahul",
+              password:"priya"
+        }}  /> */}
+        
+       
         </>
     )
 }
