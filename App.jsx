eeee4d748841707dea9506bdd4df613f6c2a8aa8
kavithaTG <126230371+kavithaTG@users.noreply.github.com>
2023-03-16 13:11:13 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,19 +1,59 @@
-import React from "react";
-import Nav from "./Nav";
-import pic from "./picture.avif"
+import React from 'react'
+// import "./Color.css"
+// import json from "./Matri.json"
+import {Component} from 'react'
+// import Matrimoni from './Matrimoni'
+// import Nav from "./Nav"
+import "./Color.css"
 
-const App=()=>{
-    return(
-        <>
-        {/* <Nav data={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiBZSHpER02qdROeHLYq8lzbITfmfvlFAdY1BAQIDzMeZGCqifSwY3rNRK1Tepl2vzr7E&usqp=CAU"}/> */}
-        {/* <Nav data={pic} /> */}
-        {/* <Nav data={{
-              username:"rahul",
-              password:"priya"
-        }}  /> */}
-        
-       
-        </>
-    )
+class App extends Component{
+  constructor(){
+    super()
+    this.state={
+      count:0
+    }
+  }
+    handleIncrement= ()=>{
+      
+      this.setState({count:this.state.count+1})
+    }
+    handleDecrement= ()=>{
+      this.setState({count:this.state.count-1})
+      
+
+        }
+      
+    
+    handleReset= ()=>{
+      this.setState({count:this.state.count=0})
+    }
+  
+
+  render()
+  {
+  return(
+    <>
+    <center>
+    <div className="cal">
+      <div>
+      <h1>{this.state.count}</h1>
+      </div>
+  
+    <button onClick={this.handleIncrement}>+</button>
+    <button onClick={this.handleDecrement}>-</button>
+  <button className='btn' onClick={this.handleReset}>Reset</button>
+  </div>
+  </center>
+    
+  
+    
+    
+    {/* <Nav />
+      </div><Matrimoni data={this.state.json}/> */}
+    </>
+  )
+  }
 }
-export default App;
+
+
+ export default App;
